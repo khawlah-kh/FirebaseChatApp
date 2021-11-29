e86d4c1bcc0b649d34e21c75188b0916320889d8
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2021-11-29 07:47:53 +0300

    Initial Commit

---
+++ FirebaseChatApp/FirebaseChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatAppApp.swift
+//  FirebaseChatApp
+//
+//  Created by khawlah khalid on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
